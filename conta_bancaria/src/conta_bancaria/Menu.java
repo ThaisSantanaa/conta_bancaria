229c9345240975210a0755eb10769d85346aab38
ThaisSantanaa <thaislimasantana9@gmail.com>
2026-06-23 11:59:07 -0300

    :sparkles:Construção da Model Conta Corrente

--- conta_bancaria/src/conta_bancaria/Menu.java
+++ conta_bancaria/src/conta_bancaria/Menu.java
@@ -3,6 +3,8 @@ package conta_bancaria;
 import java.util.Scanner;
 
 import conta_bancaria.model.Conta;
+import conta_bancaria.model.ContaCorrente;
+import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.util.Cores;
 
 public class Menu {
@@ -10,32 +12,45 @@ public class Menu {
 				
 				Scanner leia = new Scanner(System.in);
 				
-	/* Instanciar Objetos da Classe Conta - SACAR*/
-	Conta c1 = new Conta(1, 123, 1, "Isabella", 200000.00f);
+	/* Instanciar Objetos da Classe Conta */
+	/*Conta c1 = new Conta(1, 123, 1, "Isabella", 200000.00f);
 	 c1.visualizar();
 				
 	Conta c2 = new Conta(2, 123, 2, "Thiago", 100000.00f);
-	 c2.visualizar();
+	 c2.visualizar();*/
 				
 				
 	/* Alteração do Saldo*/
-	c1.setSaldo(300000.00f);
-	c1.visualizar();
+	/*c1.setSaldo(300000.00f);
+	c1.visualizar(); */
 	
 	
 	/*If ternário
 	 * 
 	 * condição ? ação se for verdadeiro : se for falso*/ 
 		
-	 System.out.println("\nSacar R$ 3000000.00 da conta C2: " + (c2.sacar(1000.00f) ? 
+	 /*System.out.println("\nSacar R$ 3000000.00 da conta C2: " + (c2.sacar(1000.00f) ? 
 						"Saque efetuado com sucesso!" : "Saldo Insuficiente"));
 	 c2.visualizar();
 		
 				
 	/*Deposito*/
 	    
-    c2.depositar(50000.00f);
+    /*c2.depositar(50000.00f);
     c2.visualizar();
+    
+    /*Instanciar Objetos da ContaConrrente*/
+    
+    /*ContaCorrente cc1 = new ContaCorrente(3, 789, 1, "Raquel", 200000.00f, 2000.00f);
+    cc1.visualizar();*/
+    
+    /*System.out.println("\nSacar R$ 202.000,00 da conta C2: " + (cc1.sacar(203000.00f) ? 
+	"Saque efetuado com sucesso!" : "Saldo Insuficiente"));
+    cc1.visualizar();*/
+    
+    ContaPoupanca cp1 = new ContaPoupanca(3, 789, 2, "Raquel", 200000.00f, 25);
+    cp1.visualizar();
+    
 				
 				
 				
